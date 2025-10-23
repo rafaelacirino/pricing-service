0be9244aad5c726a2e70164b14900b4ab25fe210
rafaelacirino <rafaelaborbaf@gmail.com>
2025-10-23 13:42:20 +0200

    adding data

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE prices (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    brand_id BIGINT,
+    start_date TIMESTAMP,
+    end_date TIMESTAMP,
+    price_list INTEGER,
+    product_id BIGINT,
+    priority INTEGER,
+    price DECIMAL(10,2),
+    curr VARCHAR(3)
+);
