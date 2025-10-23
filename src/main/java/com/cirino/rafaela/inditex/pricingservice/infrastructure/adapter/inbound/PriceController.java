022a39a97936e1cc54ed8e04b1fc4bab8c98e535
rafaelacirino <rafaelaborbaf@gmail.com>
2025-10-23 13:42:48 +0200

    adding Controller (endpoint)

---
+++ src/main/java/com/cirino/rafaela/inditex/pricingservice/infrastructure/adapter/inbound/PriceController.java
@@ -0,0 +1,33 @@
+package com.cirino.rafaela.inditex.pricingservice.infrastructure.adapter.inbound;
+
+import com.cirino.rafaela.inditex.pricingservice.application.dto.PriceResponseDto;
+import com.cirino.rafaela.inditex.pricingservice.application.ports.inbound.GetPriceUseCase;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/prices")
+public class PriceController {
+
+    private final GetPriceUseCase getPriceUseCase;
+
+    public PriceController(GetPriceUseCase getPriceUseCase) {
+        this.getPriceUseCase = getPriceUseCase;
+    }
+
+    @GetMapping
+    public ResponseEntity<PriceResponseDto> getPrice(
+            @RequestParam("applicationDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime applicationDate,
+            @RequestParam("productId") Long productId,
+            @RequestParam("brandId") Long brandId) {
+
+        PriceResponseDto response = getPriceUseCase.getPrice(applicationDate, productId, brandId);
+        return ResponseEntity.ok(response);
+    }
+}
