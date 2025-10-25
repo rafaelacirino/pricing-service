09a716c29cfc8be6d272c93f1c0b344995698253
rafaelacirino <rafaelaborbaf@gmail.com>
2025-10-25 10:07:22 +0200

    add integration test and GlobalExceptionHandler

--- src/test/java/com/cirino/rafaela/inditex/pricingservice/PricingServiceApplicationTests.java
+++ src/test/java/com/cirino/rafaela/inditex/pricingservice/PricingServiceApplicationTests.java
@@ -1,14 +1,17 @@
 package com.cirino.rafaela.inditex.pricingservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
 
-@SpringBootTest
 class PricingServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-        // empty class
-	}
-
+    @Test
+    void applicationContextLoads() {
+        try (MockedStatic<SpringApplication> mockedStatic = Mockito.mockStatic(SpringApplication.class)) {
+            PricingServiceApplication.main(new String[]{});
+            mockedStatic.verify(() -> SpringApplication.run(PricingServiceApplication.class, new String[]{}));
+        }
+    }
 }
